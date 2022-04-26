92494986bf551fd7053fda8bf1ace96c8b7fa6b3
alinanefedowa <nefedowa.as@gmail.com>
2022-04-26 13:41:49 +0300

    add build.gradle and gitignore

---
+++ src/test/java/guru/qa/GitHubTests.java
@@ -0,0 +1,29 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTests {
+    @Test
+    public void testIssueSearch() {
+
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("selenide");
+        $(".header-search-input").submit();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("Soft");
+        $(By.linkText("SoftAssertions")).shouldBe(visible).click();
+        $$(".markdown-body").findBy(visible).shouldHave(text("Using JUnit5 extend test class"));
+    }
+
+}
+
+//Задание
+//Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+//Разница есть.
